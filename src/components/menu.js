1fb803f914c98811c076f73dc40982fb64bb3f74
ghilas <1779458@crosemont.qc.ca>
2022-04-01 17:39:11 -0400

    refactorisation css

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 
 import itemsList from '../components/itemList';
-import {containerCard, card, priceTag, container, photo} from "../components/layout.module.css" 
+import {containerCard, containerCardName, cardTop, containerCardPrice, card, container, photo} from "../components/layout.module.css" 
 const Menu = () => {
     return (
         <div className={container}>
@@ -10,26 +10,28 @@ const Menu = () => {
             return(
               <div className={card}>
                 <h4 key={id}>
-                  <div>
-                    <img src={path} alt={name}  className={photo}/>
-                  </div>
-                  <div className={containerCard}>
-                    <h4>
-                      <span>
-                        {name}
-                      </span>
-                    </h4>
-                    <h4  className={priceTag}>
-                      <span className={priceTag} >
-                        {price} $
-                      </span>  
-                    </h4>
-                    <p>
-                      <span>
-                        {desc}
-                      </span>
-                    </p>
-                  </div>
+                  <div >
+                    <div className={cardTop}>
+                      <img src={path} alt={name} className={photo} />
+                    </div>
+                    <div className={containerCard}>
+                      <h4 className={containerCardName}>
+                        <span>
+                          {name}
+                        </span>
+                      </h4>
+                      <h4  className={containerCardPrice}>
+                        <span>
+                          {price} $
+                        </span>  
+                      </h4>
+                      <p>
+                        <span>
+                          {desc}
+                        </span>
+                      </p>
+                    </div>
+                  </div>  
                 </h4>
               </div>
 
