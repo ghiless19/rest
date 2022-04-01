2234d3ce7d9014a4fa9b9f985631a07a5239b0f8
ghilas <1779458@crosemont.qc.ca>
2022-04-01 13:58:13 -0400

    refactorisation data items

--- src/components/itemList.js
+++ src/components/itemList.js
@@ -1,38 +1,43 @@
+import chicken from "../images/chicken.jpeg"
+import couscous from "../images/couscous.jpeg"
+import rice from "../images/rice.png"
+import pizza from "../images/pizza.jpeg"
+import menu_salade from "../images/menu_salade.jpeg"
 
 const itemsList = [
     {
         id: 1,
         name: 'Chicken',
         price: 11.99,
-        path: '../images/chicken.jpeg',
+        path: chicken,
         desc: `A Chicken freshly made by the MultiCuisine Restaurant `,
       },
       {
         id: 2,
         name: 'Couscous',
         price: 11.99,
-        path: '../images/couscous.jpeg',
+        path: couscous,
         desc: `A couscous freshly made by the MultiCuisine Restaurant `,
       },
       {
         id: 3,
         name: 'Spicey Rice ',
         price: 7.99,
-        path: '../images/rice.png',
+        path: rice,
         desc: `A Spicey Rice freshly made by the MultiCuisine Restaurant `,
       },
       {
         id: 4,
         name: 'Cheesy Pizza',
         price: 15.99,
-        path: '../images/pizza.jpeg',
+        path: pizza,
         desc: ` A Cheesy Pizza freshly made by the MultiCuisine Restaurant`,
       },
       {
         id: 5,
-        name: 'buttermilk Salade',
+        name: 'Cesar Salade',
         price: 11.99,
-        path: '../images/menu_salade.jpeg',
+        path: menu_salade,
         desc: `A Menu Salade freshly made by the MultiCuisine Restaurant `,
       },
     
